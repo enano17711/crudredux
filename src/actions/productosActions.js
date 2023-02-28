94ce717aa8dfdbfac1b3bffed1d626c5516656dd
enano17711 <enano17711@gmail.com>
2023-02-28 11:14:31 -0400

    add_product ok

---
+++ src/actions/productosActions.js
@@ -0,0 +1,25 @@
+import {AGREGAR_PRODUCTO, AGREGAR_PRODUCTO_EXITO} from "../types/index.js";
+
+export function crearNuevoProductoAction(producto) {
+    return (dispatch) => {
+        dispatch(agregarProducto())
+
+        try {
+            dispatch(agregarProductoExito(producto))
+        } catch (e) {
+            dispatch(agregarProductoError(true))
+        }
+    }
+}
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+})
+const agregarProductoExito = (producto) => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto
+})
+const agregarProductoError = () => {
+
+}
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from "react-redux";
+import {crearNuevoProductoAction} from "../actions/productosActions.js";
 
 const NuevoProducto = () => {
+    const [nombre, setNombre] = useState('')
+    const [precio, setPrecio] = useState(0)
+
+    const dispatch = useDispatch()
+    const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto))
+
+    const submitNuevoProducto = (e) => {
+        e.preventDefault()
+
+        if (nombre.trim() === '' || precio <= 0) return
+
+        agregarProducto({nombre, precio})
+    };
+
     return (
         <div className='row justify-content-center'>
             <div className='col-md-8'>
@@ -10,17 +26,17 @@ const NuevoProducto = () => {
                             Agregar Nuevo Producto
                         </h2>
 
-                        <form>
+                        <form onSubmit={submitNuevoProducto}>
                             <div className='form-group'>
                                 <label htmlFor="">Nombre Producto</label>
                                 <input type="text" className='form-control' placeholder='Nombre Producto'
-                                       name='nombre'/>
+                                       name='nombre' value={nombre} onChange={e => setNombre(e.target.value)}/>
                             </div>
 
                             <div className='form-group'>
                                 <label htmlFor="">Precio Producto</label>
                                 <input type="number" className='form-control' placeholder='Nombre Producto'
-                                       name='precio'/>
+                                       name='precio' value={precio} onChange={e => setPrecio(Number(e.target.value))}/>
                             </div>
 
                             <button type='submit'
